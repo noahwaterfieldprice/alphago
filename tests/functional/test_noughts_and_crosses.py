7619561642f0f2622d7ac8a20348798cf5a2be33
noahwaterfieldprice <noahwaterfieldprice@gmail.com>
2018-02-09 16:50:21 +0000

    MAINT: Refactor NoughtsAndCrossesState into simpler module level functions

--- tests/functional/test_noughts_and_crosses.py
+++ tests/functional/test_noughts_and_crosses.py
@@ -1,29 +1,36 @@
 import numpy as np
 import pytest
 
-from alphago.noughts_and_crosses import NoughtsAndCrossesState
+import alphago.noughts_and_crosses as nac
 
 
 def test_can_create_initial_state():
-    assert NoughtsAndCrossesState.initial_state == (np.nan, ) * 9
+    assert nac.INITIAL_STATE == (np.nan,) * 9
 
 
 def test_correctly_identifies_state_terminality():
     terminal_state = (1, 1, 1, -1, 1, -1, 1, -1, -1)
-    non_terminal_state = (1, -1, np.nan, np.nan, 1, -1, np.nan, -1, 1)
+    non_terminal_state = (1, -1, np.nan, np.nan, -1, 1, np.nan, 1, -1)
 
-    assert NoughtsAndCrossesState.is_terminal(terminal_state)
-    assert not NoughtsAndCrossesState.is_terminal(non_terminal_state)
+    assert nac.is_terminal(terminal_state)
+    assert not nac.is_terminal(non_terminal_state)
 
 
 def test_exception_raised_when_utility_called_for_non_terminal_state():
-    non_terminal_state = (1, -1, np.nan, np.nan, 1, -1, np.nan, -1, 1)
+    non_terminal_state = (1, -1, np.nan, np.nan, -1, 1, np.nan, 1, -1)
 
-    with pytest.raises(ValueError):
-        NoughtsAndCrossesState.utility(non_terminal_state)
+    with pytest.raises(ValueError) as exception_info:
+        nac.utility(non_terminal_state)
+
+    assert str(exception_info.value) == ("Utility can not be calculated "
+                                         "for a non-terminal state.")
 
 
-def test_calculating_utility():
+def test_calculating_utility_of_terminal_state():
     terminal_state_p1_wins = (1, 1, 1, -1, 1, -1, 1, -1, -1)
-    terminal_state_p2_wins = (1, 1, 1, -1, 1, -1, 1, -1, -1)
-    pass
+    terminal_state_p2_wins = (-1, -1, -1, 1, -1, 1, -1, 1, 1)
+    terminal_state_draw = (1, 1, -1, -1, -1, 1, 1, -1, 1)
+
+    assert nac.utility(terminal_state_p1_wins) == nac.Outcome(1, -1)
+    assert nac.utility(terminal_state_p2_wins) == nac.Outcome(-1, 1)
+    assert nac.utility(terminal_state_draw) == nac.Outcome(0, 0)
--- tests/unit/noughts_and_crosses_test.py
+++ tests/unit/noughts_and_crosses_test.py
@@ -4,38 +4,59 @@ import pytest
 from alphago import noughts_and_crosses as nac
 
 terminal_states = [
-    (1, 1, 1, 1, -1, 1, -1, -1, -1),  # 1s top line
-    (1, -1, 1, 1, -1, -1, 1, -1, -1),  # 1s left side
+    (1, 1, 1, 1, -1, -1, -1, -1, 1),  # 1s top line
     (1, -1, -1, -1, 1, 1, -1, 1, 1),  # 1s negative diagonal
     (1, 1, 1, 1, -1, -1, 1, -1, -1),  # 1s top line and left side
-    (1, -1, 1, -1, 1, -1, 1, -1, 1)   # 1s both diagonals
+    (1, -1, 1, -1, 1, -1, 1, -1, 1),  # 1s both diagonals
+    (-1, 1, np.nan, -1, 1, 1, -1, -1, 1),  # -1s left side
+    (1, np.nan, -1, -1, -1, 1, -1, 1, 1),  # -1s positive diagonal
+    (1, 1, -1, -1, -1, 1, 1, -1, 1),  # draw
 ]
 
-def test_abstract_state():
-    abstract_state = nac.AbstractState
+outcomes = ([nac.Outcome(1, -1)] * 4 +
+            [nac.Outcome(-1, 1)] * 2 +
+            [nac.Outcome(0, 0)])
 
-    assert abstract_state.initial_state is None  # TODO: Maybe this is a bad idea
+line_sums_list = [
+    (3, -1, -1, 1, -1, 1, 1, -1),
+    (-1, 1, 1, -1, 1, 1, 3, -1),
+    (3, -1, -1, 3, -1, -1, -1, 1),
+    (1, -1, 1, 1, -1, 1, 3, 3),
+    (0, 1, -1, -3, 1, 2, 1, 0),
+    (0, -1, 1, -1, 0, 1, 1, -3),
+    (1, -1, 1, 1, -1, 1, 1, -1),
+]
 
-    with pytest.raises(NotImplementedError):
-        abstract_state.vector(None)
-
-    with pytest.raises(NotImplementedError):
-        abstract_state.available_actions(None)
-
-    with pytest.raises(NotImplementedError):
-        abstract_state.next_state(None, None)
-
-    with pytest.raises(NotImplementedError):
-        abstract_state.is_terminal(None)
-
-    with pytest.raises(NotImplementedError):
-        abstract_state.utility(None)
+non_terminal_states = [
+    (1, -1, np.nan, np.nan, 1, np.nan, 1, np.nan, -1),
+    (np.nan, 1, -1, -1, 1, -1, 1, np.nan, 1),
+    (1, np.nan, 1, np.nan, -1, np.nan, 1, np.nan, -1),
+]
 
 
 def test_noughts_and_crosses_initial_state():
-    assert nac.NoughtsAndCrossesState.initial_state == (np.nan, ) * 9
+    assert nac.INITIAL_STATE == (np.nan,) * 9
 
 
-@pytest.mark.parametrize("terminal_state", terminal_states)
-def test_is_terminal_returns_true_for_terminal_states(terminal_state):
-    assert nac.NoughtsAndCrossesState.is_terminal(terminal_state)
+@pytest.mark.parametrize("state", terminal_states)
+def test_is_terminal_returns_true_for_terminal_states(state):
+    assert nac.is_terminal(state)
+
+
+@pytest.mark.parametrize("state, outcome", zip(terminal_states, outcomes))
+def test_utility_function_returns_correct_outcomes(state, outcome):
+    assert nac.utility(state) == outcome
+
+
+@pytest.mark.parametrize("state, line_sums",
+                         zip(terminal_states, line_sums_list))
+def test_line_sums_are_calculated_correctly(state, line_sums):
+    assert tuple(nac._calculate_line_sums(state)) == line_sums
+
+
+@pytest.mark.parametrize("state", non_terminal_states)
+def test_utility_raises_exception_on_non_terminal_input_state(state):
+    with pytest.raises(ValueError) as exception_info:
+        nac.utility(state)
+    assert str(exception_info.value) == ("Utility can not be calculated "
+                                         "for a non-terminal state.")
