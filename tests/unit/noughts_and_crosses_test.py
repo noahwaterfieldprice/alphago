0c612459cadda346e226b023cb834f6d55d965c5
noahwaterfieldprice <noahwaterfieldprice@gmail.com>
2018-02-09 10:35:12 +0000

    TST: Tests for determining noughts and crosses terminal state

--- tests/unit/noughts_and_crosses_test.py
+++ tests/unit/noughts_and_crosses_test.py
@@ -1,10 +1,18 @@
 import pytest
 
-from alphago import noughts_and_crosses
+from alphago import noughts_and_crosses as nac
+
+terminal_states = [
+    (1, 1, 1, 1, 0, 1, 0, 0, 0),  # 1s top line
+    (1, 0, 1, 1, 0, 0, 1, 0, 0),  # 1s left side
+    (1, 0, 0, 0, 1, 1, 0, 1, 1),  # 1s negative diagonal
+    (1, 1, 1, 1, 0, 0, 1, 0, 0),  # 1s top line and left side
+    (1, 0, 1, 0, 1, 0, 1, 0, 1)   # 1s both diagonals
+]
 
 
 def test_abstract_state():
-    abstract_state = noughts_and_crosses.AbstractState
+    abstract_state = nac.AbstractState
 
     assert abstract_state.initial_state == None  # TODO: Maybe this is a bad idea
 
@@ -22,3 +30,12 @@ def test_abstract_state():
 
     with pytest.raises(NotImplementedError):
         abstract_state.utility()
+
+
+def test_noughts_and_crosses_initial_state():
+    assert nac.initial_state == (None, ) * 9
+
+
+@pytest.mark.parametrize("terminal_state", terminal_states)
+def test_is_terminal_returns_true_for_terminal_states(terminal_state):
+    assert nac.NoughtsAndCrosses.is_terminal(terminal_state)
