362a5d78dc0dd959c1dde04e6bb0c26039499d62
Chris Nicholls <cgnicholls@googlemail.com>
2018-02-09 10:39:44 +0000

    ENH: Made AbstractState unit tests pass.

--- tests/unit/noughts_and_crosses_test.py
+++ tests/unit/noughts_and_crosses_test.py
@@ -1,24 +1,24 @@
 import pytest
 
-from alphago import noughts_and_crosses
+from alphago import noughts_and_crosses as nac
 
 
 def test_abstract_state():
-    abstract_state = noughts_and_crosses.AbstractState
+    abstract_state = nac.AbstractState
 
     assert abstract_state.initial_state == None  # TODO: Maybe this is a bad idea
 
     with pytest.raises(NotImplementedError):
-        abstract_state.vector()
+        abstract_state.vector(None)
 
     with pytest.raises(NotImplementedError):
-        abstract_state.available_actions()
+        abstract_state.available_actions(None)
 
     with pytest.raises(NotImplementedError):
-        abstract_state.next_state()
+        abstract_state.next_state(None, None)
 
     with pytest.raises(NotImplementedError):
-        abstract_state.is_terminal()
+        abstract_state.is_terminal(None)
 
     with pytest.raises(NotImplementedError):
-        abstract_state.utility()
+        abstract_state.utility(None)
