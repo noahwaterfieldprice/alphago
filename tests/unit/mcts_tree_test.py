38dd721da3391f444fb24533e881b9569d86e8a3
Chris Nicholls <cgnicholls@googlemail.com>
2018-02-10 11:55:30 +0000

    MAINT: Refactored MCTSTree to be functions in a module.

--- tests/unit/mcts_tree_test.py
+++ tests/unit/mcts_tree_test.py
@@ -2,35 +2,35 @@
 import numpy as np
 import pytest
 
-from alphago.mcts_tree import MCTSNode, MCTSTree
+from alphago import mcts_tree
 
 
 def test_mcts_tree_initial_tree():
-    mcts_tree = MCTSTree(None)
+    root = mcts_tree.MCTSNode(None, None)
     
     # Check the root has prior_prob None
-    assert mcts_tree.root.prior_prob is None
-    assert mcts_tree.root.Q == 0.0
-    assert mcts_tree.root.W == 0.0
-    assert mcts_tree.root.N == 0
+    assert root.prior_prob is None
+    assert root.Q == 0.0
+    assert root.W == 0.0
+    assert root.N == 0
 
-    assert len(mcts_tree.root.children) == 0
+    assert len(root.children) == 0
 
-    assert mcts_tree.root.game_state is None
+    assert root.game_state is None
 
 def test_mcts_tree_is_leaf():
-    leaf_node = MCTSNode(None, None)
-    assert MCTSTree.is_leaf(leaf_node)
+    leaf_node = mcts_tree.MCTSNode(None, None)
+    assert mcts_tree.is_leaf(leaf_node)
 
 def test_mcts_tree_expand_root():
     # Check we can expand the root of the tree.
-    mcts_tree = MCTSTree(1)
+    root = mcts_tree.MCTSNode(None, 1)
 
     children = {'a': 2, 'b': 3}
     prior_probs = {'a': 0.4, 'b': 0.6}
 
-    leaf = mcts_tree.root
-    MCTSTree.expand(leaf, prior_probs, children)
+    leaf = root
+    mcts_tree.expand(leaf, prior_probs, children)
 
     assert leaf.children['a'].game_state == 2
     assert leaf.children['a'].prior_prob == 0.4
@@ -38,18 +38,18 @@ def test_mcts_tree_expand_root():
     assert leaf.children['b'].prior_prob == 0.6
 
 def test_mcts_tree_selects_root_as_leaf():
-    mcts_tree = MCTSTree(1)
+    root = mcts_tree.MCTSNode(None, 1)
 
-    nodes, actions = mcts_tree.select(10, 10)
+    nodes, actions = mcts_tree.select(root, 10, 10)
     assert len(actions) == 0
     assert len(nodes) == 1
-    assert nodes[0] == mcts_tree.root
+    assert nodes[0] == root
 
 
 backup_nodes = [
-    [MCTSNode(None, 3)],
-    [MCTSNode(None, None), MCTSNode(None, None)],
-    [MCTSNode({'a': 1.0}, 1), MCTSNode({3: 0.5}, 2)],
+    [mcts_tree.MCTSNode(None, 3)],
+    [mcts_tree.MCTSNode(None, None), mcts_tree.MCTSNode(None, None)],
+    [mcts_tree.MCTSNode({'a': 1.0}, 1), mcts_tree.MCTSNode({3: 0.5}, 2)],
 ]
 
 backup_values = [
@@ -60,16 +60,16 @@ backup_values = [
 
 @pytest.mark.parametrize("nodes, v", zip(backup_nodes, backup_values))
 def test_mcts_backup(nodes, v):
-    MCTSTree.backup(nodes, v)
+    mcts_tree.backup(nodes, v)
     for node in nodes:
         assert node.N == 1.0
         assert node.W == v
         assert node.Q == v
 
 backup_nodes_n_times = [
-    [MCTSNode(None, 3)],
-    [MCTSNode(None, None), MCTSNode(None, None)],
-    [MCTSNode({'a': 1.0}, 1), MCTSNode({3: 0.5}, 2)],
+    [mcts_tree.MCTSNode(None, 3)],
+    [mcts_tree.MCTSNode(None, None), mcts_tree.MCTSNode(None, None)],
+    [mcts_tree.MCTSNode({'a': 1.0}, 1), mcts_tree.MCTSNode({3: 0.5}, 2)],
 ]
 
 backup_n = [
@@ -81,22 +81,21 @@ backup_n = [
 @pytest.mark.parametrize("nodes, v, n", zip(backup_nodes_n_times, backup_values, backup_n))
 def test_mcts_backup_twice_n_times(nodes, v, n):
     for i in range(n):
-        MCTSTree.backup(nodes, v)
+        mcts_tree.backup(nodes, v)
     for node in nodes:
         assert node.N == float(n)
         assert node.W == n*v
         assert node.Q == v
     
 def test_mcts_select():
-    mcts_tree = MCTSTree(1)
+    root = mcts_tree.MCTSNode(None, 1)
 
     # Manually create a small tree below root.
-    root = mcts_tree.root
-    root.children = {'a': MCTSNode(0.2, 2), 'b': MCTSNode(0.8, 3)}
+    root.children = {'a': mcts_tree.MCTSNode(0.2, 2), 'b': mcts_tree.MCTSNode(0.8, 3)}
     root.prior_prob = 1.0
     root.N = 4
     childa = root.children['a']
-    childa.children = {'c': MCTSNode(0.7, 4), 'd': MCTSNode(0.3, 5)}
+    childa.children = {'c': mcts_tree.MCTSNode(0.7, 4), 'd': mcts_tree.MCTSNode(0.3, 5)}
     childa.N = 2
 
     nodec = childa.children['c']
@@ -105,7 +104,7 @@ def test_mcts_select():
     noded.N = 1
 
     childb = root.children['b']
-    childb.children = {'e': MCTSNode(0.9, 6), 'f': MCTSNode(0.1, 7)}
+    childb.children = {'e': mcts_tree.MCTSNode(0.9, 6), 'f': mcts_tree.MCTSNode(0.1, 7)}
     childb.N = 1
 
     nodee = childb.children['e']
@@ -113,7 +112,7 @@ def test_mcts_select():
     nodef = childb.children['f']
     nodef.N = 1
 
-    nodes, actions = mcts_tree.select(10, 1.0)
+    nodes, actions = mcts_tree.select(root, 10, 1.0)
     # We should go through the root, then choose child 'b', then child 'e'.
     # This gives game states 1, 3, 6.
     assert [node.game_state for node in nodes] == [1, 3, 6]
@@ -125,5 +124,5 @@ def test_compute_ucb():
     action_counts = {'a': 10, 'b': 20, 'c': 30}
     num = np.sqrt(sum(action_counts.values()))
     expected = {a: action_values[a] + prior_probs[a] / (1.0 + action_counts[a]) * c_puct * num for a in action_values}
-    computed = MCTSTree.compute_ucb(action_values, prior_probs, action_counts, c_puct)
+    computed = mcts_tree.compute_ucb(action_values, prior_probs, action_counts, c_puct)
     assert expected == computed
