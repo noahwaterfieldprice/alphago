e0f81cd4ae6aadce00e698299b2f370a5eaf9fdc
Chris Nicholls <cgnicholls@googlemail.com>
2018-03-19 21:58:26 +0000

    ENH: Added backwards induction to solve small games.

--- examples/alphago_noughts_and_crosses_comparator.py
+++ examples/alphago_noughts_and_crosses_comparator.py
@@ -1,6 +1,6 @@
 import alphago.games.noughts_and_crosses as nac
 import alphago.games.connect_four as cf
-from alphago import mcts_tree
+from alphago import alphago
 from alphago.evaluator import create_trivial_evaluator, BasicNACNet
 from alphago.evaluator import BasicConnectFourNet
 from alphago import comparator
@@ -21,19 +21,13 @@ if __name__ == "__main__":
     for i in range(50):
         # Train net 1, then compare against net 2.
         print("Training net 1")
-        mcts_tree.alpha_go(evaluator1, net1.train, game.ACTION_INDICES,
-                           game.compute_next_states, game.INITIAL_STATE,
-                           game.utility, game.which_player, game.is_terminal,
-                           self_play_iters=100, mcts_iters=mcts_iters,
-                           c_puct=1.0)
+        alphago.alphago(evaluator1, net1.train, game.ACTION_INDICES, game,
+                        self_play_iters=100, mcts_iters=mcts_iters,
+                        c_puct=1.0)
 
         print("We are player 1.")
-        comparator.compare(
-            game.compute_next_states, game.INITIAL_STATE, game.utility,
-            game.which_player, game.is_terminal, evaluator1, evaluator2,
-            mcts_iters=mcts_iters, num_games=50)
+        comparator.compare(game, evaluator1, evaluator2,
+                           mcts_iters=mcts_iters, num_games=50)
         print("We are player 2.")
-        comparator.compare(
-            game.compute_next_states, game.INITIAL_STATE, game.utility,
-            game.which_player, game.is_terminal, evaluator2, evaluator1,
-            mcts_iters=mcts_iters, num_games=50)
+        comparator.compare(game, evaluator2, evaluator1,
+                           mcts_iters=mcts_iters, num_games=50)
