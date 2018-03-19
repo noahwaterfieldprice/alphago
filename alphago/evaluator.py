e896289001c0cff0a3804deb0e52eaf195213bac
Chris Nicholls <cgnicholls@googlemail.com>
2018-03-19 22:17:12 +0000

    ENH: Added comparison to optimal play in example. Also changed to using Momentum rather than SGD

--- alphago/evaluator.py
+++ alphago/evaluator.py
@@ -45,7 +45,7 @@ def create_trivial_evaluator(next_states_function):
 
 
 class BasicNACNet:
-    def __init__(self, learning_rate=1e-2):
+    def __init__(self, learning_rate=1e-3):
         self.learning_rate = learning_rate
         self.tensors = self._initialise_net()
 
@@ -116,7 +116,8 @@ class BasicNACNet:
 
             # Set up the training op
             self.train_op = \
-                tf.train.GradientDescentOptimizer(self.learning_rate).minimize(loss)
+                tf.train.MomentumOptimizer(self.learning_rate,
+                                           momentum=0.9).minimize(loss)
 
             # Initialise all variables
             self.sess.run(tf.global_variables_initializer())
@@ -298,7 +299,8 @@ class BasicConnectFourNet:
 
             # Set up the training op
             self.train_op = \
-                tf.train.GradientDescentOptimizer(self.learning_rate).minimize(loss)
+                tf.train.MomentumOptimizer(self.learning_rate,
+                                           momentum=0.9).minimize(loss)
 
             # Initialise all variables
             self.sess.run(tf.global_variables_initializer())
