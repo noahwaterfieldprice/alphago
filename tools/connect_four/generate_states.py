3d10c041308d46bef8ed23f90f03fe64bc006cc9
Chris Nicholls <cgnicholls@googlemail.com>
2018-05-23 22:47:04 +0100

    ENH: Added generate random states function for Connect Four.

---
+++ tools/connect_four/generate_states.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+from alphago.games.connect_four import action_list_to_state, ConnectFour
+
+
+
+def generate_states(n, min_length=1):
+    """Generates n random connect four states, uniformly in length, and then
+    uniformly across states of that length.
+
+    Parameters
+    ----------
+    n: int
+        The number of states to generate.
+
+    Returns
+    -------
+    states: set
+        A set of states.
+    """
+    states = set()
+    game = ConnectFour()
+    while len(states) <= n:
+        l = np.random.randint(min_length, 43)
+        state = np.random.choice(7, l)
+
+        state_array = action_list_to_state(state)
+
+        if not game.is_terminal(state_array):
+            states.add(tuple(state + 1))
+
+    return states
+
+
+if __name__ == "__main__":
+    states = generate_states(100000, 8)
+
+    with open('output.txt', 'w') as f:
+        string_states = [''.join((str(x) for x in state)) for state in states]
+        f.write('\n'.join(string_states))
