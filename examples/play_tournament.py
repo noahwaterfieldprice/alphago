80e8b95f8ae0c35242b5e2bf75af652a6bf71403
noahwaterfieldprice <noahwaterfieldprice@gmail.com>
2018-06-01 12:07:48 +0100

    ENH: Add compare against players function and update SL example.

--- examples/play_tournament.py
+++ examples/play_tournament.py
@@ -1,5 +1,5 @@
 from alphago.games import NoughtsAndCrosses, ConnectFour
-from alphago.evaluator import run_tournament
+from alphago.evaluator import run_tournament, compare_against_players
 from alphago.player import RandomPlayer, MCTSPlayer
 from alphago.estimator import create_trivial_estimator, create_rollout_estimator
 from alphago.elo import elo
@@ -18,7 +18,7 @@ rollout_estimator_10 = create_rollout_estimator(game, 10)
 rollout_estimator_100 = create_rollout_estimator(game, 100)
 rollout_estimator_200 = create_rollout_estimator(game, 200)
 
-mcts_args = 50, 0.5, 0.01
+mcts_args = 10, 0.5, 0.01
 random_player = RandomPlayer(game)
 trivial_mcts_player = MCTSPlayer(game, trivial_estimator, *mcts_args)
 rollout_mcts_player_10 = MCTSPlayer(game, rollout_estimator_10, *mcts_args)
@@ -26,14 +26,16 @@ rollout_mcts_player_100 = MCTSPlayer(game, rollout_estimator_100, *mcts_args)
 rollout_mcts_player_200 = MCTSPlayer(game, rollout_estimator_200, *mcts_args)
 
 players = {
-    1: random_player,
-    2: trivial_mcts_player,
-    3: rollout_mcts_player_10,
-    4: rollout_mcts_player_100,
-    #5: rollout_mcts_player_200
+    2: random_player,
+    3: trivial_mcts_player,
+    4: rollout_mcts_player_10,
+    # 4: rollout_mcts_player_100,
+    # 5: rollout_mcts_player_200
 }
 
-results_list = run_tournament(game, players, 5)
+# results_list = run_tournament(game, players, 5)
+results_list = compare_against_players(game, (1, rollout_mcts_player_100),
+                                       players, 5)
 print(results_list)
 
 results = np.zeros(shape=(len(players), len(players)))
@@ -47,4 +49,5 @@ ax.text(0.4, -0.8, str(["{:d}: {:.2f}".format(player_no, gamma)
                         for player_no, gamma in sorted(gammas.items())]))
 a = ax.matshow(results, cmap=plt.cm.coolwarm)
 plt.colorbar(a)
+plt.tight_layout()
 fig.savefig('results.png')
--- examples/train_connect_four_sl.py
+++ examples/train_connect_four_sl.py
@@ -1,15 +1,20 @@
 """This file trains a connect four net with supervised learning.
 """
 import argparse
+import collections
+import json
 import time
 
 import numpy as np
 import tensorflow as tf
 
 from alphago.games.connect_four import action_list_to_state, ConnectFour
-from alphago.estimator import ConnectFourNet
+from alphago.estimator import (ConnectFourNet, create_trivial_estimator,
+                               create_rollout_estimator)
+from alphago.evaluator import compare_against_players
 from alphago.alphago import optimise_estimator
 from tools.summary_scalars import SummaryScalars
+from alphago.player import RandomPlayer, MCTSPlayer
 
 
 def compute_checkpoint_name(step, path):
@@ -71,14 +76,30 @@ if __name__ == "__main__":
     dev_data = training_data[:num_dev]
     training_data = training_data[num_dev:]
 
+    # Comparison players for evaluation
+    game = ConnectFour()
+    trivial_estimator = create_trivial_estimator(game)
+    rollout_estimator = create_rollout_estimator(game, 50)
+    random_player = RandomPlayer(game)
+    trivial_mcts_player = MCTSPlayer(game, trivial_estimator, 500, 0.5, 0.01)
+    rollout_mcts_player = MCTSPlayer(game, rollout_estimator, 500, 0.5, 0.01)
+    fixed_comparison_players = {1: random_player,
+                                2: trivial_mcts_player,
+                                3: rollout_mcts_player}
+
+    #results_list = json.loads("results")
+    supervised_player_no = 4
+    supervised_players_queue = collections.deque(maxlen=2)
+
     # Hyperparameters
     learning_rate = 1e-4
     batch_size = 32
     l2_weight = 1e-1
     value_weight = 1e-2
     num_train = len(training_data)
+    evaluate_every = 5
 
-    checkpoint_every = 30
+    checkpoint_every = 5
     num_steps = 1000
 
     # Build the hyperparameter string
@@ -86,12 +107,12 @@ if __name__ == "__main__":
         "lr={},batch_size={},value_weight={},l2_weight={},num_train={}").format(
         learning_rate, batch_size, value_weight, l2_weight, num_train)
 
-    game = ConnectFour()
     game_name = 'connect_four-sl'
 
     current_time_format = time.strftime('%Y-%m-%d_%H:%M:%S')
     path = "experiments/{}-{}-{}/".format(game_name, hyp_string, current_time_format)
     checkpoint_path = path + 'checkpoints/'
+    game_results_path = path + "game_results.json"
 
     estimator = ConnectFourNet(learning_rate=learning_rate,
                                l2_weight=l2_weight, value_weight=value_weight,
@@ -119,10 +140,35 @@ if __name__ == "__main__":
         summary_scalars.run({'dev_loss': dev_loss,
                              'dev_loss_value': dev_loss_value,
                              'dev_loss_probs': dev_loss_probs},
-                             estimator.global_step, writer)
+                            estimator.global_step, writer)
 
         if step % checkpoint_every == 0:
             checkpoint_name = compute_checkpoint_name(step, checkpoint_path)
             estimator.save(checkpoint_name)
 
+            new_estimator = ConnectFourNet(learning_rate=learning_rate,
+                                           l2_weight=l2_weight, value_weight=value_weight,
+                                           action_indices=game.action_indices)
+            new_estimator.restore(checkpoint_name)
 
+            new_player = MCTSPlayer(game, new_estimator, 500, 0.5)
+
+            supervised_players = {j: player for j, player
+                                  in supervised_players_queue}
+            comparison_players = {**fixed_comparison_players,
+                                  **supervised_players}
+
+            game_results = compare_against_players(game, (6, new_player),
+                                                   comparison_players)
+
+            # load results from file
+            results = json.load(game_results_path)
+            results.update(game_results)
+            with open(game_results_path, 'w') as f:
+                json.dump(results, game_results_path)
+
+
+            # elo to writer
+
+            supervised_players_queue.appendleft((supervised_player_no, new_player))
+            supervised_player_no += 1
