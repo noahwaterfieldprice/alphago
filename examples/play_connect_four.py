8fdf51bde638ad59355552abaaa30e21a1317996
Chris Nicholls <cgnicholls@googlemail.com>
2018-08-02 15:10:49 +0100

    ENH: Can now play connect four with a trained network as the estimator

--- examples/play_connect_four.py
+++ examples/play_connect_four.py
@@ -1,27 +1,57 @@
 """This program plays connect four using Monte Carlo Tree Search and a trivial
-evaluator. For nonterminal states, the evaluator returns the uniform probability
-distribution over available actions and a value of 0. In a terminal state, we
-back up the utility returned by the game.
+estimator. For nonterminal states, the evaluator returns the uniform
+probability distribution over available actions and a value of 0. In a
+terminal state, we back up the utility returned by the game.
 """
 
+import argparse
 
 import numpy as np
 
 from alphago.games.connect_four import ConnectFour
+from alphago.estimator import ConnectFourNet
 from alphago import mcts_tree
 from alphago.estimator import create_trivial_estimator
 
-if __name__ == "__main__":
 
-    max_iters = 1000
-    c_puct = 0.5
+def load_net(checkpoint):
+    """Load the network at the checkpoint.
 
+    Parameters
+    ----------
+    checkpoint: str
+        Should be the name of the checkpoint, including the path. Probably
+        ends in '.checkpoint'.
+
+    Returns
+    -------
+    AbstractNeuralNetEstimator
+        The network loaded.
+    """
+    game = ConnectFour()
+    estimator = ConnectFourNet(learning_rate=1e-4,
+                               l2_weight=1e-4, value_weight=0.01,
+                               action_indices=game.action_indices)
+    estimator.restore(checkpoint)
+    return estimator
+
+
+def play_game(human, estimator, mcts_iters, c_puct):
+    """
+
+    Parameters
+    ----------
+    human: int
+        1 if human plays first, otherwise 2 and human plays second.
+
+    Returns
+    -------
+    int
+        Result of the game: 1 if you win, 0 if you draw, -1 if you lose.
+    """
     cf = ConnectFour()
-    evaluator = create_trivial_estimator(cf.legal_actions)
-
     state = cf.initial_state
-    computer = np.random.choice([1, 2])
-    human = 1 if computer == 2 else 2
+    computer = 1 if human == 2 else 2
     print("You are player: {}".format(human))
     while not cf.is_terminal(state):
         player = cf.current_player(state)
@@ -29,7 +59,7 @@ if __name__ == "__main__":
         if player == computer:
             root = mcts_tree.MCTSNode(state, player)
             action_probs = mcts_tree.mcts(
-                root, evaluator, cf,  max_iters, c_puct)
+                root, cf, estimator, mcts_iters, c_puct)
             actions, probs = zip(*action_probs.items())
             print("Action probabilities: {}".format(action_probs))
             action_ix = np.random.choice(range(len(actions)), p=probs)
@@ -38,15 +68,15 @@ if __name__ == "__main__":
         else:
             action = None
             while action not in next_states:
-                action_ix = int(input("Your move (0-6 reading "
+                action_ix = int(input("Your move (1-7 reading "
                                       "across the board): "))
+                action_ix -= 1
                 if 0 <= action_ix < len(cf.action_space):
                     action = cf.action_space[action_ix]
         state = next_states[action]
 
         cf.display(state)
         print("\n")
-
     # The state is terminal, so let's see who won.
     utility = cf.utility(state)
     if utility[human] == 1:
@@ -55,3 +85,32 @@ if __name__ == "__main__":
         print("It's a draw!")
     else:
         print("You lose!")
+    return utility[human]
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--player', help='1 if you play first, 2 if you play '
+                                         'second.')
+    parser.add_argument('--checkpoint',
+                        help='The checkpoint path to use for the estimator. '
+                             'If not given, then use a trivial estimator.')
+
+    args = parser.parse_args()
+
+    mcts_iters = 1000
+    c_puct = 0.5
+
+    if args.player:
+        human = int(args.player)
+    else:
+        human = np.random.choice([1, 2])
+
+    if args.checkpoint:
+        estimator = load_net(args.checkpoint)
+    else:
+        cf = ConnectFour()
+        estimator = create_trivial_estimator(cf)
+
+    play_game(human, estimator, mcts_iters, c_puct)
