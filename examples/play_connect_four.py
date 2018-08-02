4a7bba6d9758ccf1d2f2d7ae964b5d5d48021ee8
Chris Nicholls <cgnicholls@googlemail.com>
2018-08-02 15:57:00 +0100

    ENH: Can now type 'cheat' for the optimal moves in the position.

--- examples/play_connect_four.py
+++ examples/play_connect_four.py
@@ -8,7 +8,7 @@ import argparse
 
 import numpy as np
 
-from alphago.games.connect_four import ConnectFour
+from alphago.games.connect_four import ConnectFour, optimal_moves
 from alphago.estimator import ConnectFourNet
 from alphago import mcts_tree
 from alphago.estimator import create_trivial_estimator
@@ -36,7 +36,7 @@ def load_net(checkpoint):
     return estimator
 
 
-def play_game(human, estimator, mcts_iters, c_puct):
+def play_game(human, estimator, mcts_iters, c_puct, tau):
     """
 
     Parameters
@@ -53,6 +53,7 @@ def play_game(human, estimator, mcts_iters, c_puct):
     state = cf.initial_state
     computer = 1 if human == 2 else 2
     print("You are player: {}".format(human))
+    action_list = []
     while not cf.is_terminal(state):
         player = cf.current_player(state)
         next_states = cf.legal_actions(state)
@@ -64,24 +65,32 @@ def play_game(human, estimator, mcts_iters, c_puct):
                 action = max(action_probs, key=action_probs.get)
             else:
                 action_probs = mcts_tree.mcts(
-                    root, cf, estimator, mcts_iters, c_puct)
+                    root, cf, estimator, mcts_iters=mcts_iters, c_puct=c_puct,
+                    tau=tau)
                 actions, probs = zip(*action_probs.items())
                 print("Action probabilities: {}".format(action_probs))
                 action_ix = np.random.choice(range(len(actions)), p=probs)
                 action = actions[action_ix]
-            print("Taking action: {}".format(action))
+            print("Taking action: {}".format(action + 1))
         else:
             action = None
             while action not in next_states:
-                action_ix = int(input("Your move (1-7 reading "
-                                      "across the board): "))
+                user_input = input("Your move (1-7 reading across the "
+                                   "board): ")
+                if user_input == 'cheat':
+                    print("Optimal moves: {}".format(optimal_moves(
+                          action_list)))
+                    continue
+                action_ix = int(user_input)
                 action_ix -= 1
                 if 0 <= action_ix < len(cf.action_space):
                     action = cf.action_space[action_ix]
+        action_list.append(action + 1)
         state = next_states[action]
 
         cf.display(state)
         print("\n")
+        print("Action list: {}".format("".join(map(str, action_list))))
     # The state is terminal, so let's see who won.
     utility = cf.utility(state)
     if utility[human] == 1:
@@ -103,13 +112,17 @@ if __name__ == "__main__":
                              'If not given, then use a trivial estimator.')
     parser.add_argument('--mcts_iters', help='If 0, then just use the raw '
                                              'network.')
+    parser.add_argument('--tau', help='Defaults to 1. Set closer to 0 for '
+                                      'more exploitation.')
+    parser.add_argument('--c_puct', help='Defaults to 0.5')
 
     args = parser.parse_args()
 
     mcts_iters = int(args.mcts_iters) if args.mcts_iters is not None else 1000
-    c_puct = 0.5
+    tau = float(args.tau) if args.tau is not None else 1
+    c_puct = float(args.c_puct) if args.c_puct is not None else 0.5
 
-    if args.player:
+    if args.player is not None:
         human = int(args.player)
     else:
         human = np.random.choice([1, 2])
@@ -120,4 +133,4 @@ if __name__ == "__main__":
         cf = ConnectFour()
         estimator = create_trivial_estimator(cf)
 
-    play_game(human, estimator, mcts_iters, c_puct)
+    play_game(human, estimator, mcts_iters, c_puct, tau)
